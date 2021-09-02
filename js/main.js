10c2bef9c9b486bf44fe376e09de42459dbcf622
Hinh1009 <dinhhinh10091998@gmail.com>
2021-09-02 08:52:57 +0700

    complete task

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 const leftArrow = document.querySelector(".bx.bx-left-arrow-alt");
-
 const rightArrow = document.querySelector(".bx.bx-right-arrow-alt");
-
 const carrouselContainer = document.querySelector(".carrousels-container");
-
 const carrousels = document.querySelector(".carrousels").children;
-
 const dotBtn = document.getElementsByClassName("dot-icon");
+const projectContainer = document.querySelector(".projects");
+const projects = document.querySelector(".project-container").children;
+const projectDotBtn = document.getElementsByClassName("dot");
 
 const totalCarrousels = carrousels.length;
 
@@ -16,7 +15,9 @@ const time = 3000;
 
 let index = 0;
 
-let slideId;
+let projectIndex = 0;
+
+let slideId, projectId;
 
 let changeCarrousel = (direction) => {
   if (direction == "next") {
@@ -32,12 +33,10 @@ let changeCarrousel = (direction) => {
       index--;
     }
   }
-
   for (i = 0; i < totalCarrousels; i++) {
     carrousels[i].classList.add("hidden");
     dotBtn[i].classList.remove("active");
   }
-
   carrousels[index].classList.remove("hidden");
   dotBtn[index].classList.add("active");
 };
@@ -48,11 +47,31 @@ let autoPlay = () => {
   }, time);
 };
 
+let projectAutoPlay = () => {
+  projectId = setInterval(() => {
+    projectIndex == projects.length - 1
+      ? (projectIndex = 0)
+      : (projectIndex += 1);
+    for (i = 0; i < projects.length; i++) {
+      projects[i].classList.add("hidden");
+      projectDotBtn[i].classList.remove("active");
+    }
+    projects[projectIndex].classList.remove("hidden");
+    projectDotBtn[projectIndex].classList.add("active");
+  }, time);
+};
+
 carrouselContainer.addEventListener("mouseenter", () => {
   clearInterval(slideId);
 });
 
 carrouselContainer.addEventListener("mouseleave", autoPlay);
+
+projectContainer.addEventListener("mouseenter", () => {
+  clearInterval(projectId);
+});
+
+projectContainer.addEventListener("mouseleave", projectAutoPlay);
 
 leftArrow.addEventListener("click", () => {
   changeCarrousel("prev");
@@ -64,16 +83,30 @@ rightArrow.addEventListener("click", () => {
 
 for (i = 0; i < dotBtn.length; i++) {
   dotBtn[i].addEventListener("click", (e) => {
+    e.preventDefault();
     index = parseInt(e.target.getAttribute("value")) - 1;
-
     for (i = 0; i < totalCarrousels; i++) {
       carrousels[i].classList.add("hidden");
       dotBtn[i].classList.remove("active");
     }
-
     carrousels[index].classList.remove("hidden");
     dotBtn[index].classList.add("active");
   });
 }
 
+for (i = 0; i < projectDotBtn.length; i++) {
+  projectDotBtn[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    projectIndex = parseInt(e.target.getAttribute("value")) - 1;
+    for (i = 0; i < projects.length; i++) {
+      projects[i].classList.add("hidden");
+      projectDotBtn[i].classList.remove("active");
+    }
+    projects[projectIndex].classList.remove("hidden");
+    projectDotBtn[projectIndex].classList.add("active");
+  });
+}
+
 autoPlay();
+
+projectAutoPlay();
